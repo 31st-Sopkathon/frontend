0a6ca98e1faa8474ff0ea48e87ae38b6daea78d8
wooyoung0830 <thddndud0830@gmail.com>
2022-11-20 06:39:19 +0900

    feat : select result O/X 버튼 기능

--- src/util/lib/api.ts
+++ src/util/lib/api.ts
@@ -3,3 +3,11 @@ import { reqAPI } from './index';
 export const getUser = async (params: string) => {
   return reqAPI.get(`/${params}`);
 };
+
+export const getXInformation = async (introductionId: number) => {
+  return reqAPI.get(`/x-introduction/${introductionId}`);
+};
+
+export const postXInformationStatus = async (introductionId: number, status: boolean) => {
+  return reqAPI.patch(`/x-introduction/${introductionId}`, status);
+};
