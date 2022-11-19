721f33bff8ec51318dc4dde624c0454d3741ac9f
seohee0112 <ghdtj7@naver.com>
2022-11-20 06:41:40 +0900

    style : view icon 구현

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,4 @@
-export { ReactComponent as ICNoriLogo } from './noriLogoIcon.svg';
+export { ReactComponent as ICDownloadBtn } from './download.svg';
+export { ReactComponent as ICCorrectRect } from './ic_correct_rec.svg';
+export { ReactComponent as ICNextBtn } from './ic_nextBtn.svg';
+export { ReactComponent as ICLetterTop } from './ic_triangle.svg';
