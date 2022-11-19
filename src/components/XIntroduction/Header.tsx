10983f764a82c7e4e7ba86ad39b52d4c7e8809d9
seohee0112 <ghdtj7@naver.com>
2022-11-20 06:42:32 +0900

    feat : 헤더 구현

--- src/components/XIntroduction/Header.tsx
+++ src/components/XIntroduction/Header.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Header = () => {
-  return <StHeaderWrapper>나의 X를 소개합니다</StHeaderWrapper>;
+  return <StHeaderWrapper>나의 X를 소개합니다.</StHeaderWrapper>;
 };
 
 export default Header;
@@ -10,13 +10,17 @@ const StHeaderWrapper = styled.header`
   display: flex;
   align-items: center;
   text-align: center;
-  letter-spacing: 0.1em;
+
+  margin-top: 6.15rem;
+  margin-bottom: 10.1rem;
 
   font-family: 'S-Core Dream';
   font-style: normal;
-  font-weight: 500;
-  font-size: 2rem;
-  line-height: 2.4rem;
+  font-weight: 600;
+  font-size: 2.2rem;
+  line-height: 2.6rem;
+
+  color: #853636;
 
   color: #853636;
 `;
