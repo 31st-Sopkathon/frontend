eb6e98b31fa5605ae3e3e36d0d8542661b32cef9
[talkingOrange] <sonrisa-bonita@naver.com>
2022-11-20 06:39:11 +0900

    #1/login+inform UI

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,3 @@
+export { ReactComponent as IcCalendar } from './IcCalendar.svg';
+export { ReactComponent as IcLoginLogo } from './IcLoginLogo.svg';
 export { ReactComponent as ICNoriLogo } from './noriLogoIcon.svg';
