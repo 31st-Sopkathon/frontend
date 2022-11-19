856746c5b07027708114c854089d6cf573355da6
seohee0112 <ghdtj7@naver.com>
2022-11-20 07:19:50 +0900

    fix : lint 버그 수정

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,12 +1,9 @@
-
-export { ReactComponent as ICNoriLogo } from './noriLogoIcon.svg';
-export { ReactComponent as XLogoIcon } from './xLogoIcon.svg';
-export { ReactComponent as XSelectBtn } from './xSelectBtn.svg';
 export { ReactComponent as ICDownloadBtn } from './download.svg';
 export { ReactComponent as ICCorrectRect } from './ic_correct_rec.svg';
 export { ReactComponent as ICNextBtn } from './ic_nextBtn.svg';
 export { ReactComponent as ICLetterTop } from './ic_triangle.svg';
 export { ReactComponent as IcCalendar } from './IcCalendar.svg';
 export { ReactComponent as IcLoginLogo } from './IcLoginLogo.svg';
+export { ReactComponent as ICNoriLogo } from './noriLogoIcon.svg';
 export { ReactComponent as XLogoIcon } from './xLogoIcon.svg';
-
+export { ReactComponent as XSelectBtn } from './xSelectBtn.svg';
